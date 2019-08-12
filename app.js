8a7247632cbc2d1f2c0fe018dc0ec401d42e18fd
Jacob Bankston <jacob.bankston.smile@gmail.com>
2019-08-12 14:42:38 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Ability to create a new post
+// Ability to delete a post
+// Ability to update a post
+// Ability to view all posts
+
+// !Ability to add user accounts
+// !Ability to allow login
+// !Ability to associate users with their own content
+
